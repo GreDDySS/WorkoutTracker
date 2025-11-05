a3fa9154a814daf71a114ffa5ebdec97b87ee668
GreDDySS <misha.sudos@yandex.ru>
2025-11-05 12:53:32 +0800

    Model for Timer

---
+++ WorkoutTracker/Models/WorkoutTimer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutTracker.Models
+{
+    public class WorkoutTimer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Sets { get; set; }
+        public int WorkDuration { get; set; }
+        public int RestInterval { get; set; }
+    }
+}
