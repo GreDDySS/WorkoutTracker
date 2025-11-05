17ba5b5b402ada39cfba0fc0fa8d99c36da74502
GreDDySS <misha.sudos@yandex.ru>
2025-11-05 12:53:07 +0800

    Initialize file sqllite

---
+++ WorkoutTracker/Data/WorkoutDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkoutTracker.Models;
+using System.IO;
+
+namespace WorkoutTracker.Data
+{
+    public class WorkoutDbContext : DbContext
+    {
+        public DbSet<WorkoutTimer> Timers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "workout.db");
+            
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
