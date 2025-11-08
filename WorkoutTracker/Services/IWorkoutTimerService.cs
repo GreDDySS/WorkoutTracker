50bdbf9b543841f212417b8243e57eefa260ab07
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:44:07 +0800

    feat: add IWorkoutTimerService interface for timer operations

---
+++ WorkoutTracker/Services/IWorkoutTimerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Services
+{
+    public interface IWorkoutTimerService
+    {
+        event EventHandler<int> TimeElapsed;
+        event EventHandler PhaseCompleted;
+
+        void Start();
+        void Pause();
+        void Resume();
+        void Stop();
+        void Dispose();
+    }
+}
