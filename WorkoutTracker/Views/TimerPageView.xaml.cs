6ecd37306ae512ea25ea6f27334b658d8217decb
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:47:03 +0800

    refactor: update TimerPageView to use dependency injection

--- WorkoutTracker/Views/TimerPageView.xaml.cs
+++ WorkoutTracker/Views/TimerPageView.xaml.cs
@@ -1,9 +1,36 @@
+using WorkoutTracker.Models;
+using WorkoutTracker.Services;
+using WorkoutTracker.ViewModels;
+
 namespace WorkoutTracker.Views;
 
 public partial class TimerPageView : ContentPage
 {
-	public TimerPageView()
+    private TimerViewModel _viewModel;
+
+    public TimerPageView()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        WorkoutSettings settings = AppShell.NavigationSettings ?? new WorkoutSettings();
+        AppShell.NavigationSettings = null; 
+
+        var timerService = Handler.MauiContext.Services.GetService<IWorkoutTimerService>();
+        var stateService = Handler.MauiContext.Services.GetService<IWorkoutStateService>();
+        var navigationService = Handler.MauiContext.Services.GetService<INavigationService>();
+
+        _viewModel = new TimerViewModel(timerService, stateService, navigationService, settings);
+        BindingContext = _viewModel;
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _viewModel?.Dispose();
+    }
 }
