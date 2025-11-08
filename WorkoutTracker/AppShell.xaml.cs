0cd7492c1e0cdd69236f9dddca00e1b1f5136f09
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:47:57 +0800

    Test tabbar

--- WorkoutTracker/AppShell.xaml.cs
+++ WorkoutTracker/AppShell.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace WorkoutTracker
+﻿using WorkoutTracker.Models;
+using WorkoutTracker.Views;
+
+namespace WorkoutTracker
 {
     public partial class AppShell : Shell
     {
+        public static WorkoutSettings NavigationSettings { get; set; }
+
         public AppShell()
         {
             InitializeComponent();
+            
+            // Register route for TimerPage
+            Routing.RegisterRoute("TimerPage", typeof(TimerPageView));
         }
     }
 }
