dda401bd3f03b4aa26f7f5692639430c1f8931e9
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:45:01 +0800

    feat: add AddExerciseViewModel for exercise management

---
+++ WorkoutTracker/ViewModels/AddExerciseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WorkoutTracker.Views;
+
+namespace WorkoutTracker.ViewModels
+{
+     public class AddExerciseViewModel : BaseViewModel
+    {
+        public ICommand SaveExerciseCommand { get; set; }
+
+        public AddExerciseViewModel()
+        {
+            SaveExerciseCommand = new Command(SaveExercise);
+        }
+
+        public void SaveExercise()
+        {
+            Application.Current.MainPage = new CreateWorkoutPageView();
+        }
+    }
+}
