e8cf8ee81272e4841b3d8a519157ebe8f729329d
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:43:13 +0800

    feat: add WorkoutState model for tracking workout progress

---
+++ WorkoutTracker/Models/WorkoutState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker.Models
+{
+    public class WorkoutState
+    {
+        public int CurrentApproach { get; set; } = 1;
+        public bool IsWorkPhase { get; set; } = true;
+        public int CurrentTimeSeconds { get; set; }
+        public bool IsPaused { get; set; } = false;
+        public bool IsCompleted { get; set; } = false;
+
+        public void Reset(WorkoutSettings settings)
+        {
+            CurrentApproach = 1;
+            IsWorkPhase = true;
+            CurrentTimeSeconds = settings.WorkTimeSeconds;
+            IsCompleted = false;
+        }
+    }
+}
