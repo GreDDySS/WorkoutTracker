66d89259d164fabdd33ab1b589d30dadf62d9b53
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:44:30 +0800

    feat: add TimeFormatter utility class for time formatting

---
+++ WorkoutTracker/Services/TimeFormatter.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutTracker.Services
+{
+    public static class TimeFormatter
+    {
+        public static string FormatTime(int totalSeconds)
+        {
+            int minutes = totalSeconds / 60;
+            int seconds = totalSeconds % 60;
+            return $"{minutes:D2}:{seconds:D2}";
+        }
+    }
+}
