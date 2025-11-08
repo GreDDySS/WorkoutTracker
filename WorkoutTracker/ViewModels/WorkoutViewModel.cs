edef753f4f195b58a682a64f80b867afea2528bb
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:45:59 +0800

    feat: add WorkoutViewModel for workout management

---
+++ WorkoutTracker/ViewModels/WorkoutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WorkoutTracker.Views;
+
+namespace WorkoutTracker.ViewModels
+{
+    public class WorkoutViewModel : BaseViewModel
+    {
+
+        public ICommand CreateWorkoutCommand { get; set; }
+
+        public WorkoutViewModel()
+        {
+            CreateWorkoutCommand = new Command(CreateWorkout);
+
+        }
+        
+        private void CreateWorkout()
+        {
+            Application.Current.MainPage = new CreateWorkoutPageView();
+        }
+
+    }
+}
