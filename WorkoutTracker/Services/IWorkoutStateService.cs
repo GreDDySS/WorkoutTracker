19be45903555a1b0891066a06d453f86b670a920
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:43:55 +0800

    feat: add IWorkoutStateService interface for state management

---
+++ WorkoutTracker/Services/IWorkoutStateService.cs
@@ -0,0 +1,18 @@
+﻿using WorkoutTracker.Models;
+
+namespace WorkoutTracker.Services
+{
+    public interface IWorkoutStateService
+    {
+        WorkoutState CurrentState { get; }
+        WorkoutSettings Settings { get; }
+
+        void Initialize(WorkoutSettings settings);
+        void MoveToNextPhase();
+        void MoveToPreviousPhase();
+        void DecrementTime();
+        bool IsWorkoutCompleted();
+        int CalculateRemainingTime();
+        string GetProgressText();
+    }
+}
