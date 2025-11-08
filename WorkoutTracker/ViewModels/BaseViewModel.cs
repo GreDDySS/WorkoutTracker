36b890d15a0c386aae87920ba7858bf21a26ba55
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 02:45:14 +0800

    refactor: update BaseViewModel with property change notification

--- WorkoutTracker/ViewModels/BaseViewModel.cs
+++ WorkoutTracker/ViewModels/BaseViewModel.cs
@@ -7,5 +7,13 @@ namespace WorkoutTracker.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
