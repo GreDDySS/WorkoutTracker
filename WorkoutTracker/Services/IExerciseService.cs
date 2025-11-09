e6ce273f7e236174fa406a9bba863091a44c2611
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:40:20 +0800

    feat: add exercise service interface

---
+++ WorkoutTracker/Services/IExerciseService.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutTracker.Models;
+
+namespace WorkoutTracker.Services
+{
+    public interface IExerciseService
+    {
+        Task<List<Exercise>> GetAllExercisesAsync();
+        Task<List<Exercise>> GetCustomExercisesAsync();
+        Task<List<Exercise>> GetSystemExercisesAsync();
+        Task<Exercise> GetExerciseByIdAsync(int id);
+        Task<int> SaveExerciseAsync(Exercise exercise);
+        Task<bool> DeleteExerciseAsync(int id);
+    }
+}
