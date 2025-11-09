8f1c5508e49e4f68ad5b330758532e65f8002f23
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:45:16 +0800

    feat: register new services in dependency injection container

--- WorkoutTracker/MauiProgram.cs
+++ WorkoutTracker/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using WorkoutTracker.Data;
 using WorkoutTracker.Services;
 using WorkoutTracker.ViewModels;
 
@@ -17,9 +18,16 @@ namespace WorkoutTracker
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddSingleton<WorkoutDbContext>();
+
             builder.Services.AddSingleton<IWorkoutTimerService, WorkoutTimerService>();
             builder.Services.AddTransient<IWorkoutStateService, WorkoutStateService>();
             builder.Services.AddSingleton<INavigationService, NavigationService>();
+            builder.Services.AddSingleton<IDialogService, DialogService>();
+            builder.Services.AddSingleton<IExerciseService, ExerciseService>();
+            builder.Services.AddSingleton<IProgramService, ProgramService>();
+            builder.Services.AddSingleton<ITimeAdjustmentService, TimeAdjustmentService>();
+
             builder.Services.AddTransient<TimerViewModel>();
 
 #if DEBUG
