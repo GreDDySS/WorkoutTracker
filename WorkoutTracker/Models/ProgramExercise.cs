523f0746b7186a1330240d8d43a6c44ef640cb99
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:39:25 +0800

    feat: add program-exercise relationship model

---
+++ WorkoutTracker/Models/ProgramExercise.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkoutTracker.Models
+{
+    public class ProgramExercise
+    {
+        public int Id { get; set; }
+        public int ProgramId { get; set; }
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+        public int Approaches { get; set; }
+    }
+}
