af8e1d3a86e4cf9321a8c0a0d25718da5c4162ea
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:39:48 +0800

    feat: add selectable exercise model with selection support

---
+++ WorkoutTracker/Models/SelectableExercise.cs
@@ -0,0 +1,22 @@
+﻿using WorkoutTracker.ViewModels;
+
+namespace WorkoutTracker.Models
+{
+    public class SelectableExercise : BaseViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int WorkTimeSeconds { get; set; }
+        public int RestTimeSeconds { get; set; }
+        public bool IsCustom { get; set; }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => SetProperty(ref _isSelected, value);
+        }
+
+        public string Details => $"Работа: {WorkTimeSeconds}с, Отдых: {RestTimeSeconds}с";
+    }
+}
