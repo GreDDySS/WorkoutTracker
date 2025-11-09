2d8a562248f354c64bdfb8a91f2a92463adb84bc
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:40:32 +0800

    feat: add workout program service interface

---
+++ WorkoutTracker/Services/IProgramService.cs
@@ -0,0 +1,12 @@
+﻿using WorkoutTracker.Models;
+
+namespace WorkoutTracker.Services
+{
+    public interface IProgramService
+    {
+        Task<List<Program>> GetAllProgramsAsync();
+        Task<Program> GetProgramByIdAsync(int id);
+        Task<int> SaveProgramAsync(Program program);
+        Task<bool> DeleteProgramAsync(int id);
+    }
+}
