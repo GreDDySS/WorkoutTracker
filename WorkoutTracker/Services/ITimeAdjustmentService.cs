b6d6b0d1df99f5dbbd5fdfe2cca1e9c1bc611f84
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:40:44 +0800

    feat: add time adjustment service interface

---
+++ WorkoutTracker/Services/ITimeAdjustmentService.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkoutTracker.Services
+{
+    public interface ITimeAdjustmentService
+    {
+        int AdjustTime(int currentTime, string direction, int step = 5, int minValue = 0);
+    }
+}
