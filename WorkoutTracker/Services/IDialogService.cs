d1f9611c5aca3db227da0f96bc684bc66910b38f
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:39:59 +0800

    feat: add dialog service interface

---
+++ WorkoutTracker/Services/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutTracker.Services
+{
+    public interface IDialogService
+    {
+        Task ShowAlertAsync(string title, string message, string cancel = "OK");
+        Task<bool> ShowConfirmationAsync(string title, string message, string accept = "Да", string cancel = "Нет");
+    }
+}
