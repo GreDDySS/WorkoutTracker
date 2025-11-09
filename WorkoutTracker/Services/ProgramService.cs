8e3ab75cdd4c69e7aedd2ef9eab29176133e4a99
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:41:13 +0800

    feat: implement workout program service with database stub methods

---
+++ WorkoutTracker/Services/ProgramService.cs
@@ -0,0 +1,45 @@
+﻿using WorkoutTracker.Data;
+using WorkoutTracker.Models;
+
+namespace WorkoutTracker.Services
+{
+    public class ProgramService : IProgramService
+    {
+        private readonly WorkoutDbContext _context;
+
+        public ProgramService(WorkoutDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Program>> GetAllProgramsAsync()
+        {
+            // TODO: Реализовать загрузку из БД
+            return new List<Program>();
+        }
+
+        public async Task<Program> GetProgramByIdAsync(int id)
+        {
+            // TODO: Реализовать загрузку из БД
+            return null;
+        }
+
+        public async Task<int> SaveProgramAsync(Program program)
+        {
+            // TODO: Реализовать сохранение в БД
+            System.Diagnostics.Debug.WriteLine($"Сохранение программы: {program.Name}");
+            System.Diagnostics.Debug.WriteLine($"Упражнений: {program.Exercises.Count}");
+            foreach (var ex in program.Exercises)
+            {
+                System.Diagnostics.Debug.WriteLine($"  - {ex.Exercise.Name}: {ex.Approaches} подход(ов)");
+            }
+            return 0;
+        }
+
+        public async Task<bool> DeleteProgramAsync(int id)
+        {
+            // TODO: Реализовать удаление из БД
+            return false;
+        }
+    }
+}
