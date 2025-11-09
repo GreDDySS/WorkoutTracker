c908913d8c8386f9af40477f870735da279f8aa0
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:39:07 +0800

    feat: add exercise data model

---
+++ WorkoutTracker/Models/Exercise.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutTracker.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int WorkTimeSeconds { get; set; }
+        public int RestTimeSeconds { get; set; }
+        public bool IsCustom { get; set; }
+        public int Approaches { get; set; } = 1;
+    }
+}
