56672e89595fddb31f7e13d744272281f7683e45
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:41:22 +0800

    feat: implement time adjustment service with step-based increments

---
+++ WorkoutTracker/Services/TimeAdjustmentService.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkoutTracker.Services
+{
+    public class TimeAdjustmentService : ITimeAdjustmentService
+    {
+        public int AdjustTime(int currentTime, string direction, int step = 5, int minValue = 0)
+        {
+            if (direction == "+")
+            {
+                return currentTime + step;
+            }
+            else if (direction == "-" && currentTime > minValue + step)
+            {
+                return currentTime - step;
+            }
+            return currentTime;
+        }
+    }
+}
