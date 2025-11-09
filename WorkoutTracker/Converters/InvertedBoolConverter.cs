0413848c69f01edf4e0f45e84608b78d505bc737
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:38:59 +0800

    feat: add boolean value inverter converter for XAML bindings

---
+++ WorkoutTracker/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace WorkoutTracker.Converters
+{
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
+    }
+}
