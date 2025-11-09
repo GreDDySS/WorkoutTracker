71fb4f1201fed838e1a0d74729fa9e971d1c9d14
GreDDySS <misha.sudos@yandex.ru>
2025-11-09 19:39:17 +0800

    feat: add workout program data model

---
+++ WorkoutTracker/Models/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutTracker.Models
+{
+    public class Program
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ProgramExercise> Exercises { get; set; } = new();
+    }
+}
