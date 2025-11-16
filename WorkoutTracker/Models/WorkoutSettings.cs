f5977f7814621ef51180be7ac00a5deaf58cc960
GreDDySS <misha.sudos@yandex.ru>
2025-11-16 15:19:43 +0800

    feat: Add support for programmatic exercises to workout settings

--- WorkoutTracker/Models/WorkoutSettings.cs
+++ WorkoutTracker/Models/WorkoutSettings.cs
@@ -6,6 +6,9 @@ namespace WorkoutTracker.Models
         public int Approaches { get; set; } = 1;
         public int WorkTimeSeconds { get; set; } = 20;
         public int RestTimeSeconds { get; set; } = 10;
+        public List<ProgramExerciseItem> ProgramExercises { get; set; } = null;
+
+        public bool IsProgram => ProgramExercises != null && ProgramExercises.Count > 0;
 
         public string WorkTimeDisplay => TimeFormatter.FormatTime(WorkTimeSeconds);
         public string RestTimeDisplay => TimeFormatter.FormatTime(RestTimeSeconds);
